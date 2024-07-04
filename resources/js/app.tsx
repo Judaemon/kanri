07d31ec8545a2414c45d14fa39448f0130f4b60f
Judaemon <judayajohnray@gmail.com>
2024-07-04 12:55:19 +0800

    feat: react entry point set up

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,1 +1,13 @@
-import './bootstrap';
+import "./bootstrap";
+
+import { createRoot } from "react-dom/client";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+createRoot(document.getElementById("root")!).render(<App />);
